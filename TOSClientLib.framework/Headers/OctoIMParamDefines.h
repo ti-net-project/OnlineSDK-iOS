53ea84a4da7aefbd06e90de08de3c8171c891c63
18734791527 <1142445395@qq.com>
2022-11-22 15:04:36 +0800

    v1.5.9.3

--- TOSClientLib.framework/Headers/OctoIMParamDefines.h
+++ TOSClientLib.framework/Headers/OctoIMParamDefines.h
@@ -15,10 +15,10 @@
 // 此处头文件包含内部参数的设置等
 
 #ifdef DEBUG
-#define strSDKVersion @"1.5.9.2"
+#define strSDKVersion @"1.5.9.3"
 #else
 
-#define strSDKVersion @"1.5.9.2"//todo  // 防止打包遗忘
+#define strSDKVersion @"1.5.9.3"//todo  // 防止打包遗忘
 #endif
 
 /**
