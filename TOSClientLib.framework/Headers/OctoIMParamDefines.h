f5eaf737429609945fa42eb6e9710cbf7cdee4c4
Zhao Yan <yan.zhao.cf@partner.lingyue-digital.com>
2023-06-30 16:18:07 +0800

    v1.7.11

--- TOSClientLib.framework/Headers/OctoIMParamDefines.h
+++ TOSClientLib.framework/Headers/OctoIMParamDefines.h
@@ -15,10 +15,10 @@
 // 此处头文件包含内部参数的设置等
 
 #ifdef DEBUG
-#define strSDKVersion @"1.7.10"
+#define strSDKVersion @"1.7.11"
 #else
 
-#define strSDKVersion @"1.7.10"//todo  // 防止打包遗忘
+#define strSDKVersion @"1.7.11"//todo  // 防止打包遗忘
 #endif
 
 /**
