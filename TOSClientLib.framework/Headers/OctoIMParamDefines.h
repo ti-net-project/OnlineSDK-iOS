b23b7b1ef7476455114e924e1c0f3bcb0b102f63
赵言 <1142445395@qq.com>
2023-08-28 19:38:12 +0800

    v1.8.7

--- TOSClientLib.framework/Headers/OctoIMParamDefines.h
+++ TOSClientLib.framework/Headers/OctoIMParamDefines.h
@@ -13,10 +13,10 @@
 // 此处头文件包含内部参数的设置等
 
 #ifdef DEBUG
-#define strSDKVersion @"1.8.6"
+#define strSDKVersion @"1.8.7"
 #else
 
-#define strSDKVersion @"1.8.6"//todo  // 防止打包遗忘
+#define strSDKVersion @"1.8.7"//todo  // 防止打包遗忘
 #endif
 
 /**
