b20f43b2ce2b644c5619a599315da59c23caf623
赵言 <1142445395@qq.com>
2023-09-06 11:19:20 +0800

    v1.8.9

--- TOSClientKit.framework/Headers/TIMMessageFrame.h
+++ TOSClientKit.framework/Headers/TIMMessageFrame.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import "TIMLibBaseModel.h"
+#import <TOSClientLib/TIMLibBaseModel.h>
 
 @interface TOSSatisfactionTabBarFrameModel : TIMLibBaseModel
 
