a3414eafb2eb69c79421e86df00fe65a11ec0c97
赵言 <1142445395@qq.com>
2025-02-17 11:30:06 +0800

    v2.4.3-Beta2

--- TOSClientLib.framework/Headers/OctoIMParamDefines.h
+++ TOSClientLib.framework/Headers/OctoIMParamDefines.h
@@ -13,9 +13,9 @@
 // 此处头文件包含内部参数的设置等
 
 #ifdef DEBUG
-#define strSDKVersion @"2.4.3-beta1"
+#define strSDKVersion @"2.4.3-beta2"
 #else
-#define strSDKVersion @"2.4.3-beta1"//todo  // 防止打包遗忘
+#define strSDKVersion @"2.4.3-beta2"//todo  // 防止打包遗忘
 #endif
 
 /**
